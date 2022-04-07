13fa6d834f87350e5a9ed14ee8d8d48b052b20e8
morrisonk107 <101103758+morrisonk107@users.noreply.github.com>
2022-04-07 17:41:34 +0100

    Update MORRISON_KRIS_Hangman.py

--- MORRISON_KRIS_Hangman.py
+++ MORRISON_KRIS_Hangman.py
@@ -1,152 +1,62 @@
 import random
 
-lists_word = ["rarely",
-"universe",
-"notice",
-"sugar",
-"interference",
-"constitution",
-"we",
-"minus",
-"breath",
-"clarify",
-"take",
-"recording",
-"amendment",
-"hut",
-"tip",
-"logical",
-"cast",
-"title",
-"brief",
-"none",
-"relative",
-"recently",
-"detail",
-"port",
-"such",
-"complex",
-"bath",
-"soul",
-"holder",
-"pleasant",
-"buy",
-"federal",
-"lay",
-"currently",
-"saint",
-"for",
-"simple",
-"deliberately",
-"means",
-"peace",
-"prove",
-"sexual",
-"chief",
-"department",
-"bear",
-"injection",
-"off",
-"son",
-"reflect",
-"fast",
-"ago",
-"education",
-"prison",
-"birthday",
-"variation",
-"exactly",
-"expect",
-"engine",
-"difficulty",
-"apply",
-"hero",
-"contemporary",
-"that",
-"surprised",
-"fear",
-"convert",
-"daily",
-"yours",
-"pace",
-"shot",
-"income",
-"democracy",
-"albeit",
-"genuinely",
-"commit",
-"caution",
-"try",
-"membership",
-"elderly",
-"enjoy",
-"pet",
-"detective",
-"powerful",
-"argue",
-"escape",
-"timetable",
-"proceeding",
-"sector",
-"cattle",
-"dissolve",
-"suddenly",
-"teach",
-"spring",
-"negotiation",
-"solid"
-"seek",
-"enough",
-"surface",
-"small",
-"search"]
+# PARAMETERS
+LIFE = 6
+FILE = "word_list.txt"
+display = []
+letter_already_guessed = []
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# TODO 1.Random the name from the list of words
-print("Welcome to the game of 'Hangman'. \nHere is your word to guess:")
-number = random.randint(0,2)
-word = lists_word[number]
+# TODO 1.import txt file
+file = open(FILE, "r")
+all_text = file.read()
+lists_word = list(map(str, all_text.split()))
+
+# TODO 2.Random the name from the list of words
+# Randomize the word choice and display the blanks
+word = random.choice(lists_word)
 length = len(word)
-#print(length)
-display = ["*"]* length
-print(display)
+
+# make the word into a list
 letter_list = list(word)
-#print(letter_list)
-life = 6
 
+for letter in letter_list:
+    display.append("*")
 
-#TODO 2. if player still has lifes, is the letter correct?
-#print(letter_guess)
-while life > 0 or display == letter_list:
+print(f"Welcome to the game of 'Hangman'")
+
+# TODO 3. if player still has lifes, is the letter correct?
+print(f"Here is your word to guess: {' '.join(map(str, display))}")
+while LIFE > 0:
     result = "False"
     letter_guess = input("Please, guess a letter? ").lower()
-    for letter in letter_list:
-        if letter_guess == letter:
-            
-            result = "True"
-            for item in range (0,length):
-                character = letter_list[item]
-                if character == letter_guess:
-                    display[item] = letter_guess
-                    
-#TODO 3. if player guess right or wrong? Take life or add letter to the word.           
-    if result == "False":
-        life -= 1 
-        print(f"This is not a letter in the word! You have lost a life. You have {life} lifes left.")
-        #print(result)
-        #print(life)
-        print(display)
-    elif result == "True":
-        #print(result)
-        #print(life)
-        print(display)  
-        print(f"This is a in the word! You have {life} lifes left.")
-        
+    
+    if len(letter_guess) > 1:
+        print("Please only type one character!")
+    elif letter_guess not in letters:
+        print("Please don't type characters that are not in the alphabet!")
+    elif letter_guess in letter_already_guessed:
+        print("This letter is already guessed!")
+    else:
+        letter_already_guessed.append(letter_guess)
+        for letter in letter_list:
+            if letter_guess == letter:
+                result = "True"
+                for item in range(0, length):
+                    character = letter_list[item]
+                    if character == letter_guess:
+                        display[item] = letter_guess
 
-    if life == 0:
-        print ("Game Over! Your are out of lives!")
-    elif display == letter_list:
-        print ("You Win and correctly got the word!")
-        
-        
-        
-        
+ # TODO 4. if player guess right or wrong? Take life or add letter to the word.
+        if result == "False":
+            LIFE -= 1
+            print(f"This is not a letter in the word! You have lost a life. You have {LIFE} lifes left.")
+            print(' '.join(map(str, display)))
+        elif result == "True":
+            print(' '.join(map(str, display)))
+            print(f"This is a in the word! You have {LIFE} lifes left.")
+
+        if LIFE == 0:
+            print("Game Over! Your are out of lives!")
+        elif display == letter_list:
+            print("You Win and correctly got the word!")
